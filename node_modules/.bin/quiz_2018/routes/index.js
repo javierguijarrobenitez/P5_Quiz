68391311dff61b745a3c903fa0a2610bd323d406
javier.guijarro <guijar@MacBook-Pro-de-Javier.local>
2018-05-08 13:36:22 +0200

    quiz5

---
+++ node_modules/.bin/quiz_2018/routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var Sequelize = require('sequelize');
+var sequelize = require('../models/index')
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index');
+});
+
+router.get('/credits', function(req, res, next) {
+    res.render('credits');
+});
+
+router.get('/quizzes', function(req, res, next) {
+    sequelize.models.quiz.findAll()
+        .then(quizzes => {
+        res.render('quizzes', {quizzes});
+})
+.catch(Sequelize.ValidationError, error => {
+        console.log('Quiz erróneo.');
+    error.errors.forEach(({message}) => errorlog(message));
+})
+.catch(error => {
+        console.log(error.message);
+})
+});
+
+module.exports = router;
